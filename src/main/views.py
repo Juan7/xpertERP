9590796b42e6d9e765ccf4a3a0a8f2b45884f78d
Juan7 <juan.gomez.q@gmail.com>
2016-07-27 14:55:20 -0500

    base

--- src/main/views.py
+++ src/main/views.py
@@ -1,9 +1,11 @@
 """Manage views for main app."""
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 
+@login_required
 def home(request):
     context = {
         'title': 'Home',
